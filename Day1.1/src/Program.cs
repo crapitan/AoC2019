34068ccbb67837a754190de1197b874ffacb12e5
Klas Broberg <klas.broberg@gmail.com>
2019-12-05 22:15:51 +0100

    First Part of Day 1

---
+++ Day1.1/src/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AoC2019.Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fuelCalculator = new FuelCalculator();
+            Console.WriteLine("AoCDay1.1");
+            var input = File.ReadAllLines(@"..\data\input.txt");
+            var numbers = input.Select(s => Convert.ToDecimal(s));
+
+            var totalFuleConsumtion = numbers.Sum(d => fuelCalculator.CalculateForMass(d));
+            System.Console.WriteLine($"The amount of fuel needed {totalFuleConsumtion}"); 
+        }
+    }
+
+    public class FuelCalculator
+    {
+        public decimal CalculateForMass(decimal mass)
+        {
+           return Math.Floor((mass/3) - 2);     
+        }
+    }
+
+}
---
+++ Day1.1/tests/UnitTest1.cs
@@ -0,0 +1,40 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AoC2019.Day1;
+
+namespace AoC2019.Day1.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private FuelCalculator fuelCalculator = new FuelCalculator();
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var fule = fuelCalculator.CalculateForMass(12);
+            Assert.IsTrue(fule == 2);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var fule = fuelCalculator.CalculateForMass(14);
+            Assert.IsTrue(fule == 2);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var fule = fuelCalculator.CalculateForMass(1969);
+            Assert.IsTrue(fule == 654);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var fule = fuelCalculator.CalculateForMass(100756);
+            Assert.IsTrue(fule == 33583);
+        }
+        
+    }
+}
